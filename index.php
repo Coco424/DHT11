6092eb45a9ced87453f7f3ef9c9f58971bc80be4
Corinne COUPIER <coupier.corinne@gmail.com>
2018-11-30 10:30:24 +0100

    creer main.css + modifier index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<h1>Température</h1>
 <?php
 echo "Hello World";
 ?>
+</body>
+</html>
