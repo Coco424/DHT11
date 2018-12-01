5a9b9e6a15db65f42ade367278d9357057fce328
Coco424 <coupier.corinne@gmail.com>
2018-12-01 11:40:06 +0100

    mettre derniere modif data.txt

--- index.php
+++ index.php
@@ -17,6 +17,15 @@ $bargraph_top = 315 - $json-> temperature * 4;
 ?>
 <p>Il fait <?php echo $json->temperature;?>°C avec <?php echo $json->humidite;?>% d'humidité.</p>
 
+<?php
+// Affichera : somefile.txt a été modifié le : December 29 2002 22:16:23.
+
+$filename = 'data.txt';
+if (file_exists($filename)) {
+    echo "le fichier data.txt a été modifié le : " . date ("F d Y H:i:s.", filemtime($filename));
+}
+?>
+
 <div id="thermometer">
   <div id="bargraph" style="height: <?php echo $bargraph_heigth; ?>px; top: <?php echo $bargraph_top; ?>px;"></div>
 
