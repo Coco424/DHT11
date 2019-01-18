3b72cdc442a2c84bcb8592c5125ef88e25aa4080
corinne <coupier.corinne@gmail.com>
2019-01-18 10:41:58 +0100

    ajouter nodemcu

---
+++ lcdtmp/secret.h
@@ -0,0 +1,8 @@
+//pour thinspeak
+
+unsigned long myChannelNumber = 659368;
+const char myWriteAPIKey[]="2DK8HX3K32LH8PVQ";
+
+// pour wifi
+const char ssid[] ="Hi Co";
+const char psswrd[]="ccavbv73"; 
